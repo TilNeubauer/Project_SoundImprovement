8c6d9ec1ba2e9678cbc37ba23dc38715b178dc90
TilNeubauer <nt1703@mci4me.at>
2026-02-06 13:33:45 +0100

    fix: anpassungen in main.py zum testen

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ import numpy as np
 
 import soundfile as sf
 
-from plot import plot_sig_fft_2x, plot_sig_fft, plot_only_spec, plot_sig_fft_spec_2x, plot_sig_fft_3x, plot_sig_fft_spec_3x, plot_sig_fft_noiseProfile, plot_sig_fft_spec_noiseProfile
+from plot import plot_sig_fft_2x, plot_sig_fft, plot_only_spec, plot_sig_fft_spec_2x, plot_sig_fft_3x, plot_sig_fft_spec_3x, plot_sig_fft_noiseProfile, plot_sig_fft_spec_noiseProfile, plot_only_sig
 from sinSig import gen_sin_sig, add_noise, add_sig
 from filter import denoise_fft
 from spectral_gate import noise_profile, spectral_gate
+from bandpass import butter_bandpass_filter
+from equalizer import eq_fkt, apply_eq
 
 
 
@@ -41,6 +43,7 @@ if __name__ == "__main__":
     sig: Audio sig | 1D np array
     sr: sample rate of audio file 
     """
+    #"""
     #sig1, sr1 = librosa.load(audio_path_1)
     sig_orig, sr_orig = librosa.load(audio_path_orig)
     sig_orig = sig_orig[:len(sig_orig)//5]
@@ -52,12 +55,20 @@ if __name__ == "__main__":
 
     sig = add_mismatch(sig_orig, sig_vinyl_noice)
 
-    n_fft = 2048
-    hop = 512
+    #"""
+    #n_fft = 2048
+    #hop = 512
 
+    #Bandpass 
+    y_bandpass = butter_bandpass_filter(sig, 60, 6000, sr_orig)
 
+    
+    #denoise mit spectral_gate
     f, noise_floor = noise_profile(sig_vinyl_noice, sr_vinyl_noice)            # Noise-Profil lernen
     y_clean = spectral_gate(sig, sr_orig, noise_floor)             # Gate anwenden
+
+    xs, ys = eq_fkt(e32=0, e64=0, e125=0, e250=0, e500=0, e1k=0, e2k=0, e4k=0, e8k=0, e16k=0)
+    y_eq = apply_eq(sig, sr_orig, xs, ys)
 
     #plot_sig_fft_noiseProfile(
     #                            sig1 = sig, 
@@ -71,25 +82,27 @@ if __name__ == "__main__":
     #                            )
 
 
-    plot_sig_fft_spec_noiseProfile(
-                                sig1 = sig_orig, 
-                                sr1 = sr_orig, 
-                                sig_noice = sig_vinyl_noice, 
-                                sr_noice = sr_vinyl_noice, 
-                                f = f, 
-                                noise_profile = noise_floor, 
-                                sig2 = sig, 
-                                sr2 = sr_orig,
-                                sig3 = y_clean,
-                                sr3 = sr_orig
-                                )
+    #plot_sig_fft_spec_noiseProfile(
+    #                            sig1 = sig_orig, 
+    #                            sr1 = sr_orig, 
+    #                            sig_noice = sig_vinyl_noice, 
+    #                            sr_noice = sr_vinyl_noice, 
+    #                            f = f, 
+    #                            noise_profile = noise_floor, 
+    #                            sig2 = sig, 
+    #                            sr2 = sr_orig,
+    #                            sig3 = y_clean,
+    #                            sr3 = sr_orig
+    #                            )
 
 
     #plot_sig_fft_3x(sig, sr_orig, sig_vinyl_noice, sr_vinyl_noice, y_clean, sr_orig)
 
     #plot_sig_fft_spec_3x(sig, sr_orig, sig_vinyl_noice, sr_vinyl_noice, y_clean, sr_orig)
 
-    #plot_sig_fft_2x(sig_orig, sr_orig, sig_vinyl_noice, sr_vinyl_noice)
+    #plot_sig_fft_2x(sig, sr_orig, y_bandpass, sr_orig)
+    plot_sig_fft_2x(sig, sr_orig, y_eq, sr_orig)
+
 
     #sf.write("audio_test.wav", sig, sr_orig)
     #plot_sig_fft(sig, sr_orig)
@@ -98,17 +111,21 @@ if __name__ == "__main__":
     #denoice_sig = sig - noise_sig
 
     sf.write("Sounds_out/orig_noice.wav", sig, sr_orig)
-    sf.write("Sounds_out/denoice.wav", y_clean, sr_orig)
+
+    #sf.write("Sounds_out/denoice.wav", y_clean, sr_orig)
+    #sf.write("Sounds_out/bandpass.wav", y_bandpass, sr_orig)
+    sf.write("Sounds_out/eq.wav", y_eq, sr_orig)
     #sf.write("Sounds_out/noice.wav", noise_sig, sr_orig)
 
+    #"""
 
 """
-    sr = 10000
-    sig = gen_sin_sig(1, 2, sr)
+    sr = 1000
+    sig10 = gen_sin_sig(1, 10, sr)
     #noisy = add_noise(sig, 0.2)
 
-    sig2 = add_sig(sig,10, sr)
-    sig3 = filter_freq(sig2, freq=2)
+    sig20 = add_sig(sig10,20, sr)
+    #sig3 = filter_freq(sig2, freq=2)
     #sig2 = add_sig(sig2,3.15, sr)
     #sig2 = add_sig(sig2,50, sr)
     #sig_noisy = add_noise(sig2, 1)
@@ -116,7 +133,14 @@ if __name__ == "__main__":
 
     #noice = sig_noisy - sig_denoise
 
-    plot_sig_fft_3x(sig, sr, sig2, sr, sig3, sr)
+    #plot_only_sig(sig2, sr)
+
+    sig_butter = butter_bandpass_filter(sig20,3,15,sr)
+
+    
+
+
+    plot_sig_fft_3x(sig10, sr, sig20, sr, sig_butter, sr)
     #plot_sig_fft_3x(sig_noisy, sr, sig_denoise, sr, noice, sr)
     #plot_sig_fft(sig, sr)
 """
